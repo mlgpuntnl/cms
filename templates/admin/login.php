40f44441256035757e37631e8cc00cff769628c7
mlgpuntnl <t.g.j.stevens@gmail.com>
2023-05-22 22:13:05 +0200

    Styled login form and template wrapper

--- templates/admin/login.php
+++ templates/admin/login.php
@@ -1,15 +1,13 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="/build/style.css">  -->
-    <?php $this->load_entrypoint('admin'); ?>
-    <?= $this->load_styles() ?>
-    <?= $this->load_scripts() ?>
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login page</h1>
-</body>
-</html>
+<?php $this->layout('admin/wrapper', ['title' => 'Login']) ?>
+
+<?php $this->start('main-content'); ?>
+    <div class="login-container">
+        <div class="login-form">
+            <form>
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
+<?php $this->stop();
